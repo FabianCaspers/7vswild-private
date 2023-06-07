206b33498b3384c4373ea56bf210b2fddc4c949b
FabianCaspers <fabian.c1308@icloud.com>
2023-06-07 19:20:59 +0200

    added s02

--- js/app.js
+++ js/app.js
@@ -98,3 +98,20 @@ timeline
     },
     "3"
   );
+
+  const hamburgerBtn = document.getElementById("hamburgerBtn");
+const dropdownMenu = document.querySelector(".dropdown-menu");
+
+hamburgerBtn.addEventListener("click", () => {
+  dropdownMenu.classList.toggle("show");
+});
+
+document.addEventListener("click", (event) => {
+  const target = event.target;
+  const isDropdownMenu = target.matches(".dropdown-menu") || target.closest(".dropdown-menu");
+  const isHamburgerBtn = target.matches("#hamburgerBtn") || target.closest("#hamburgerBtn");
+
+  if (!isDropdownMenu && !isHamburgerBtn) {
+    dropdownMenu.classList.remove("show");
+  }
+});
--- js/teilnehmerS2.js
+++ js/teilnehmerS2.js
@@ -114,3 +114,20 @@ for (let i = 0; i < teilnehmer.length; i++) {
 
   cardContainer.appendChild(card);
 }
+
+const hamburgerBtn = document.getElementById("hamburgerBtn");
+const dropdownMenu = document.querySelector(".dropdown-menu");
+
+hamburgerBtn.addEventListener("click", () => {
+  dropdownMenu.classList.toggle("show");
+});
+
+document.addEventListener("click", (event) => {
+  const target = event.target;
+  const isDropdownMenu = target.matches(".dropdown-menu") || target.closest(".dropdown-menu");
+  const isHamburgerBtn = target.matches("#hamburgerBtn") || target.closest("#hamburgerBtn");
+
+  if (!isDropdownMenu && !isHamburgerBtn) {
+    dropdownMenu.classList.remove("show");
+  }
+});
